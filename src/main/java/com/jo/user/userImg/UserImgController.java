4aaa012b6e4a2ea16ee8fe267692733b2463da93
jsy11618 <jsy11618@gmail.com>
2022-04-05 11:42:30 +0900

    lightHoustBackUser-userImg
    lightHoustBackUser-userImg 1st update

---
+++ src/main/java/com/jo/user/userImg/UserImgController.java
@@ -0,0 +1,41 @@
+package com.jo.user.userImg;
+
+
+import com.jo.user.userLogin.config.S3Service;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class UserImgController {
+
+    private S3Service s3Service;
+    private UserImgService userImgService;
+
+    @GetMapping("/userImg")
+    public String dispWrite(Model model) {
+        List<UserImgDto> userImgDtoList = userImgService.getUserImgList();
+
+        model.addAttribute("userImgList", userImgDtoList);
+
+        return "/userImg";
+    }
+
+    @PostMapping("/userImg")
+    public String execWrite(UserImgDto userImgDto, MultipartFile file) throws IOException {
+        String imgPath = s3Service.upload(userImgDto.getUrl(), file);
+        userImgDto.setUrl(imgPath);
+
+        userImgService.saveUser(userImgDto);
+
+        return "redirect:/userImg";
+
+    }
+}
---
+++ src/main/java/com/jo/user/userImg/UserImgDto.java
@@ -0,0 +1,33 @@
+package com.jo.user.userImg;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+public class UserImgDto {
+    private Long id;
+    private String userProfile;
+    private String url;
+    private String imgFullPath;
+
+    public UserImg toEntity(){
+        UserImg build = UserImg.builder()
+                .id(id)
+//                .userProfile(userProfile)
+                .url(url)
+                .build();
+        return build;
+    }
+
+    @Builder
+    public UserImgDto(Long id, String url, String imgFullPath) {
+        this.id = id;
+//        this.userProfile = userProfile;
+        this.url = url;
+        this.imgFullPath = imgFullPath;
+    }
+}
